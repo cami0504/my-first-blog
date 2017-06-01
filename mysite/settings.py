c1e16e336fd78265e299202aed4b2fea444f6ee1
cami0504 <kmy.05@hotmail.com>
2017-05-31 23:20:59 -0400

    Modificado las urls, y la vista

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+from django.core.urlresolvers import reverse_lazy
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -123,6 +124,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
 
 #Para autentificarme con facebook, twitter y otras aplicaciones
 '''AUTHENTICATION_BACKENDS = [
